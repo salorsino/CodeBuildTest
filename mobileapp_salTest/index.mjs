f86c03b28920fba68f2e69f0308c2af6436fbf0f
Salvatore Orsino <Salvatore.Orsino@pseg.com>
2023-06-28 11:06:00 -0400

    showing STeve the stuff

--- mobileapp_salTest/index.mjs
+++ mobileapp_salTest/index.mjs
@@ -1,5 +1,5 @@
 export const handler = async(event) => {
     console.log("This one is it");
     console.log("Stove is the man");
-    const testVar = "TestVar";
+    console.log("Saving Version & Alias")
 };
